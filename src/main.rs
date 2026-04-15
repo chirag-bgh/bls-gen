61504f4cff5247c42334279a94927e199b5245dd
chirag-bgh <chirag2bsingh@gmail.com>
2026-04-15 15:23:16 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use blst::min_pk::SecretKey;
+use rand::RngCore;
+
+pub fn gen_bls_keypair() {
+    let mut rng = rand::thread_rng();
+    let mut ikm = [0u8; 32];
+    rng.fill_bytes(&mut ikm);
+
+    let sk = SecretKey::key_gen(&ikm, &[]).unwrap();
+    let pk = sk.sk_to_pk();
+    let sk_hex = hex::encode(sk.to_bytes());
+    let pk_hex = hex::encode(pk.to_bytes());
+
+    println!("BLS Secret Key: {}, len: {}", sk_hex, sk_hex.len() / 2);
+    println!("BLS Public Key: {}, len: {}", pk_hex, pk_hex.len() / 2);
+}
+
+fn sdf() {
+    let sk = SecretKey::from
+    let pk = sk.sk_to_pk();
+    let pk_hex = hex::encode(pk.to_bytes());
+    println!("BLS Public Key: {}, len: {}", pk_hex, pk_hex.len() / 2);
+
+
+}
+fn main() {
+    sdf()
+}
